0b482dfa734a77c7c2192a47ff2a5f06431bc519
Adam Rais <raisadamo@gmail.com>
2019-05-28 18:01:50 +0900

    Add graphql folder

--- src/main.rs
+++ src/main.rs
@@ -14,43 +14,45 @@ mod schema;
 use crate::schema::{create_schema, Schema};
 
 fn graphiql() -> HttpResponse {
-    let html = graphiql_source("http://127.0.0.1:8080/graphql");
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(html)
+  let html = graphiql_source("http://127.0.0.1:8080/graphql");
+  HttpResponse::Ok()
+    .content_type("text/html; charset=utf-8")
+    .body(html)
 }
 
 fn graphql(
-    st: web::Data<Arc<Schema>>,
-    data: web::Json<GraphQLRequest>,
+  st: web::Data<Arc<Schema>>,
+  data: web::Json<GraphQLRequest>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
-        let res = data.execute(&st, &());
-        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
-    })
-    .map_err(Error::from)
-    .and_then(|user| {
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(user))
-    })
+  web::block(move || {
+    let res = data.execute(&st, &());
+    Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+  })
+  .map_err(Error::from)
+  .and_then(|user| {
+    Ok(
+      HttpResponse::Ok()
+        .content_type("application/json")
+        .body(user),
+    )
+  })
 }
 
 fn main() -> io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
+  std::env::set_var("RUST_LOG", "actix_web=info");
+  env_logger::init();
 
-    // Create Juniper schema
-    let schema = std::sync::Arc::new(create_schema());
+  // Create Juniper schema
+  let schema = std::sync::Arc::new(create_schema());
 
-    // Start http server
-    HttpServer::new(move || {
-        App::new()
-            .data(schema.clone())
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
-            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
+  // Start http server
+  HttpServer::new(move || {
+    App::new()
+      .data(schema.clone())
+      .wrap(middleware::Logger::default())
+      .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+      .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+  })
+  .bind("127.0.0.1:8080")?
+  .run()
 }
